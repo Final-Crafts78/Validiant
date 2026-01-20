b1c63d28b1c41a7badad838390e9029e40872b97
mansoorahamedsaaho <mansoorahamedsaaho@gmail.com>
2026-01-20 13:36:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3466,7 +3466,7 @@ app.get("/app.js", (req, res) => {
 
   // Apply current filter controls and fetch from /api/tasks
   function loadAllTasks() {
-    let url = "/api/tasks";
+    let url = "/api/tasks?roleadmin";
 
     const statusEl = document.getElementById("allTasksStatusFilter");
     const empEl = document.getElementById("allTasksEmployeeFilter");
